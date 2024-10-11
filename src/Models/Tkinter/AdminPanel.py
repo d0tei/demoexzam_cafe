deca64d3aabde0505099ec38b8c91a4dea64b010
d0tei <d0tei@yandex.ru>
2024-10-11 14:48:08 +0500

    07.10

---
+++ src/Models/Tkinter/AdminPanel.py
@@ -0,0 +1,93 @@
+import os
+from tkinter import *
+
+
+from src.Controllers.UserController import  UserController
+
+def admin():
+    def status_false(id):
+        button_login.configure(text = "Уволен", background='gray')
+        user.update_status(id)
+        update()
+    def add_user(login,name,passwd,role):
+        user.add(login,passwd,name,role)
+        panel_admin.after(1000,update)
+
+    def update():
+        panel_admin.destroy()
+        admin()
+    panel_admin =Tk()
+    panel_admin.geometry('1660x800')
+    panel_admin.title("Панель админстратора")
+    title_window = Label(panel_admin, text='Панель админстратора', font=("Times New Roman", 24))
+    title_window.grid(column = 0, row = 0, columnspan =12,padx = 400, pady =10)
+    title_employee = Label(panel_admin, text = "Сотрудники", font= (20))
+    title_employee.grid(column = 1, row = 1, padx = 10, pady = 10)
+    user = UserController()
+    list_user = user.get()
+    count_row = 2
+    for row in list_user:
+        if row.status:
+            login_title = Label(panel_admin, text = row.login)
+            login_title.grid(column = 0, row = count_row, padx =1, pady = 1)
+            login_role = Label(panel_admin, text = row.role_id.role)
+            login_role.grid(column = 2 ,row = count_row, padx=0,pady=1)
+            button_login = Button(panel_admin,
+                                 text = "Уволить",
+                                 height = 2,
+                                 width = 8,
+                                 background = 'red',
+                                 foreground = 'white',
+                                 #command = lambda: user.update_status(row.id)
+                                 command = lambda id = row.id: status_false(row.id))
+
+            status_tutle = Label(panel_admin, text=row.id)
+            status_tutle.grid(column=4,row=count_row,padx=0, pady=1)
+            button_login.grid(column=3, row=count_row, padx=0, pady=1)
+        else:
+            login_title = Label(panel_admin, text=row.login)
+            login_title.grid(column=0,row=count_row,padx=1,pady=1)
+            login_role = Label(panel_admin,text=row.role_id.role)
+            login_role.grid(column=2, row=count_row,padx=0,pady=1)
+            button_login = Button(panel_admin,
+                                  text="Уволен",
+                                  height=2,
+                                  width=8,
+                                  background='red',
+                                  foreground='white',
+                                  # command = lambda: user.update_status(row.id)
+                                  command=lambda id=row.id: status_false(id))
+
+            button_login.grid(column=3,row=count_row,padx=0,pady=1)
+        count_row +=1
+    input_login = Entry(panel_admin,width=20)
+    name_input_login = Label(panel_admin,text="Введите логин")
+    name_input_login.grid(column=5,row=2)
+    input_login.grid(column=5,row=3)
+
+    input_name = Entry(panel_admin,width=20)
+    name_input_name = Label(panel_admin,text="Введите имя")
+    name_input_name.grid(column=5,row=4)
+    input_name.grid(column=5,row=5)
+
+    input_password = Entry(panel_admin,width=20)
+    name_input_password = Label(panel_admin,text="Введите пароль")
+    name_input_password.grid(column=5,row=6)
+    input_password.grid(column=5,row=7)
+
+    input_role = Entry(panel_admin,width=20)
+    name_input_role = Label(panel_admin,text="Введите должность")
+    name_input_role.grid(column=5,row=8)
+    input_role.grid(column=5,row=9)
+
+    button_add_user = Button(panel_admin, text="добавить пользователя",height=2,width=20,
+                             background='green',foreground='white',
+                             command= lambda: add_user(input_login.get(),input_name.get(),
+                                                       input_password.get(),input_role.get()))
+    button_add_user.grid(column=5,row=10)
+
+    panel_admin.mainloop()
+if __name__ == '__main__':
+    admin()
+
+
